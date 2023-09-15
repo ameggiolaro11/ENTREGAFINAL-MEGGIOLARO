7e7a4aa52b38b48bc062285f3ceb48a32dddb903
ameggiolaro11 <ameggiolaro11@gmail.com>
2023-09-15 00:39:36 -0300

    ENTREGA FINAL + MEGGIOLARO

--- script.js
+++ script.js
@@ -81,48 +81,37 @@ totalContainer.textContent = `Total: $${total.toFixed(2)}`;
 }
 
 function agregarAlCarrito(index) {
-let producto = indumentariaDeportiva[index];
-if (producto.stock > 0) {
-  
-  carrito.push(producto);
-  producto.stock--;
+  let producto = indumentariaDeportiva[index];
+  if (producto.stock > 0) {
+    carrito.push(producto);
+    producto.stock--;
+    actualizarCarrito();
+    guardarEnStorage();
+  }
+  }
+
+function finalizarCompra() {
+  if (carrito.length === 0) {
+    mostrarMensaje("Tu carrito está vacío. Agrega productos antes de finalizar la compra.");
+    return;
+  }
+
+  let totalCompra = carrito.reduce((total, producto) => total + producto.precio, 0);
+  mostrarMensaje(`Monto a pagar: $${totalCompra.toFixed(2)}`);
+  mensajeContainer.textContent = "¡GRACIAS POR COMPRAR EN RASH SPORTS!";
+  mensajeContainer.classList.remove("oculto");
+  finalizarCompraButton.classList.add("oculto");
+  carrito.length = 0;
   actualizarCarrito();
   guardarEnStorage();
 }
-}
-
-let limpiarCarritoButton = document.getElementById("limpiar-carrito");
 
 function limpiarCarrito() {
-carrito = [];
-actualizarCarrito();
-guardarEnStorage();
-finalizarCompraButton.classList.add("oculto");
-mensajeContainer.classList.add("oculto");
-// carritoContainer.innerHTML = "";
-} 
-
-
-limpiarCarritoButton.addEventListener("click", limpiarCarrito);
-
-
-function finalizarCompra() {
-if (carrito.length === 0) {
-  mostrarMensaje("Tu carrito está vacío. Agrega productos antes de finalizar la compra.");
-  return;
-}
-
-let totalCompra = carrito.reduce((total, producto) => total + producto.precio, 0);
-
-mostrarMensaje(`Monto a pagar: $${totalCompra.toFixed(2)}`);
-
-
-mensajeContainer.textContent = "¡GRACIAS POR COMPRAR EN RASH SPORTS!";
-mensajeContainer.classList.remove("oculto"); 
-finalizarCompraButton.classList.add("oculto"); 
-carrito = []; 
-actualizarCarrito();
-guardarEnStorage();
+  carrito.length = 0;
+  actualizarCarrito();
+  guardarEnStorage();
+  finalizarCompraButton.classList.add("oculto");
+  mensajeContainer.classList.add("oculto");
 }
 
 function mostrarMensaje(mensaje) {
@@ -130,37 +119,70 @@ function mostrarMensaje(mensaje) {
   mensajeElement.textContent = mensaje;
 }
 
+function cargarProductos() {
+  productosContainer.innerHTML = "";
+
+  indumentariaDeportiva.forEach((producto, index) => {
+    let productoCard = document.createElement("div");
+    productoCard.className = "producto-card";
+
+    let imagen = document.createElement("img");
+    imagen.src = producto.imagen;
+    imagen.alt = producto.nombre;
+    imagen.className = "imagen";
+
+    let productoInfo = document.createElement("div");
+    productoInfo.className = "producto-info";
+    productoInfo.innerHTML = `
+      <h3>${producto.nombre}</h3>
+      <p>Precio: $${producto.precio.toFixed(2)}</p>
+      <p>Stock: ${producto.stock}</p>
+      <button onclick="agregarAlCarrito(${index})">Agregar al Carrito</button>
+    `;
+
+    productoCard.appendChild(imagen);
+    productoCard.appendChild(productoInfo);
+    productosContainer.appendChild(productoCard);
+  });
+}
+
 function guardarEnStorage() {
-localStorage.setItem("carrito", JSON.stringify(carrito));
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
 function cargarDesdeStorage() {
-const carritoGuardado = localStorage.getItem("carrito");
-if (carritoGuardado) {
-  carrito.push(...JSON.parse(carritoGuardado));
-  actualizarCarrito();
-  if (carrito.length > 0) {
-      finalizarCompraButton.classList.remove("oculto"); 
+  let carritoGuardado = localStorage.getItem("carrito");
+  if (carritoGuardado) {
+    carrito.push(...JSON.parse(carritoGuardado));
+    actualizarCarrito();
+    if (carrito.length > 0) {
+      finalizarCompraButton.classList.remove("oculto");
     }
   }
 }
 
-indumentariaDeportiva.forEach((producto, index) => {
-  let productoCard = document.createElement("div");
-  productoCard.className = "producto-card";
-  productoCard.innerHTML = `
-    <img src="${producto.imagen}" alt="${producto.nombre}" class="imagen" />
-    <div class="producto-info">
-      <h3>${producto.nombre}</h3>
-      <p>Categoría: ${producto.categoria}</p>
-      <p>Precio: $${producto.precio.toFixed(2)}</p>
-      <p>Stock: ${producto.stock}</p>
-      <button onclick="agregarAlCarrito(${index})">Agregar al carrito</button>
-    </div>
-  `;
-  productosContainer.appendChild(productoCard);
-});
-
+cargarProductos();
 cargarDesdeStorage();
 
+
 finalizarCompraButton.addEventListener("click", finalizarCompra);
+limpiarCarrito.addEventListener("click", limpiarCarrito);
+
+function cargarDatosDesdeJSONLocal() {
+  return fetch('ruta/a/tu/json.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No se pudo cargar el JSON local');
+      }
+      return response.json();
+    })
+    .then(data => {
+      
+      console.log('Datos cargados desde JSON local:', data);
+      
+    })
+    .catch(error => {
+      console.error('Error al cargar datos desde JSON local:', error);
+    });
+}
+cargarDatosDesdeJSONLocal();
